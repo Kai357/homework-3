019b907ac4fd35029a45a098ecb1708e05840d51
Kai <mordechaigerstley@gmail.com>
2021-10-13 15:20:07 -0400

    Can generate random Pass with the prompt of length

--- script.js
+++ script.js
@@ -2,12 +2,25 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+// find out how long password should be
+//
+function generatePassword() {
+  var length = prompt("Put The length of your new password here");
+  console.log(length);
+  var randomChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var randomCharsArray = randomChars.split("");
+  var result = "";
+  for (var i = 0; i < length; i++) {
+    result +=
+      randomCharsArray[Math.floor(Math.random() * randomCharsArray.length)];
+  }
+  return result;
+}
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
+  return password;
 }
 
 // Add event listener to generate button
